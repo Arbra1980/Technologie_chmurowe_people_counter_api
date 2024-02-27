250d6512c4fa152165e92903805acfec4336a675
“Arkadiusz <arkadiusz.brandt@edu.uekat.pl>
2024-02-27 23:21:51 +0100

    change main

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
-api.add_resource(HelloWorld, '/test')
+
+api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
     app.run(debug=True)
